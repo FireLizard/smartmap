5b5b160834a87d14ae459fbeb7395972725234f5
FireLizard <seb91gi@yahoo.de>
2015-07-20 22:31:28 +0200

    Add simple controller action

---
+++ src/Classes/Controller/MapController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Phoenix\Smartmap\Controller;
+
+/**
+ * Map Controller
+ */
+class MapController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+	/**
+	 * initialize action show
+	 *
+	 * @return void
+	 */
+	public function initializeShowAction() {
+
+	}
+
+	/**
+	 * action show
+	 *
+	 * @return void
+	 */
+	public function showAction() {
+
+	}
+}
