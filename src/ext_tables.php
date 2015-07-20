4fb5b004884bd80b659f9d8275b61b3fb73be2ae
FireLizard <seb91gi@yahoo.de>
2015-07-20 22:29:52 +0200

    Register FlexForms

--- src/ext_tables.php
+++ src/ext_tables.php
@@ -10,3 +10,22 @@ if (!defined('TYPO3_MODE')) {
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Smart Map');
+
+/**
+* Register FlexForms
+*/
+$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
+$pluginNames   = array(
+	'map',
+);
+
+foreach ($pluginNames as $pluginName) {
+
+	$pluginSignature = $extensionName .'_'. $pluginName;
+	$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+		$pluginSignature,
+		'FILE:EXT:'. $_EXTKEY .'/Configuration/FlexForms/'. $pluginName .'.xml'
+	);
+}
