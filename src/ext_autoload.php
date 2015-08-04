cef34d55e92962dc85b89105659825b982903fc6
Sebastian Gierth <seb91gi@yahoo.de>
2015-08-04 10:29:34 +0200

    Removed unnecessary vendors

--- src/ext_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$extPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('smartmap');
-return array(
-    'Sle\TYPO3\Extbase\Controller\BaseController' => $extPath.'vendor/Sle/TYPO3/Extbase/Controller/BaseController.php',
-);
