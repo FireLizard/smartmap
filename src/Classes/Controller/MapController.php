dee8b51b5363cc2ab84facc78821d1c5bfcf8f2e
Sebastian Gierth <seb91gi@yahoo.de>
2015-08-06 10:27:23 +0200

    Show markers

--- src/Classes/Controller/MapController.php
+++ src/Classes/Controller/MapController.php
@@ -51,6 +51,7 @@ class MapController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
         $response = array(
             'metadata' => array(
                 'settings' => $this->settings,
+                'service' => '',
             ),
             'data' => array(),
         );
@@ -60,6 +61,7 @@ class MapController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
             $provider = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance($this->settings['dataProviderClass']);
             $this->service->setDataProvider($provider);
             $response['data'] = $this->service->{$args['service']}();
+            $response['metadata']['service'] = $args['service'];
         }
 
         return json_encode($response);
--- src/Resources/Public/Js/smartmap.js
+++ src/Resources/Public/Js/smartmap.js
@@ -1,7 +1,6 @@
 var Smartmap = (function(window, document, $, undefined){
 
-    var $objects, settings;
-    var map, markers;
+    var $objects, settings, provider;
 
     function initialize() {
 
@@ -20,31 +19,75 @@ var Smartmap = (function(window, document, $, undefined){
 
             switch (settings.mapLibraryProvider) {
                 case 'leaflet':
-                    createLeafletMap();
+                    if (typeof L !== 'undefined'){
+                        provider = new mapProvider.leaflet();
+                    } else {
+                        throw 'smartmap requires Leaflet to be loaded first!';
+                    }
                     break;
+            }
+
+            if (typeof provider !== 'undefined'){
+                provider.createMap();
+
+                switch (response.metadata.service) {
+                    case 'getCoords':
+                        provider.pinMarker();
+                        break;
+                    default:
+                }
             }
         });
     }
 
-    function createLeafletMap() {
+    /**
+     * Register different map provider here.
+     */
+    var mapProvider = {
 
-        if (typeof L === 'undefined'){
-            throw 'smartmap requires Leaflet to be loaded first!';
+        /**
+         * Leaflet.
+         * @see http://leafletjs.com/reference.html
+         */
+        leaflet: function() {
+
+            var markers = [];
+            var map;
+
+            /**
+             * Creates a map.
+             * @return this
+             */
+            this.createMap = function() {
+
+                map = L.map($objects.mapContainer[0], {
+                    center: [51.06971, 13.77797],
+                    zoom: 12
+                });
+
+                L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
+                    maxZoom: 18,
+                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                    subdomains: '1234'
+                }).addTo(map);
+
+                return this;
+            };
+
+            this.pinMarker = function() {
+
+                for (var coords in data) {
+                    if (data.hasOwnProperty(coords)) {
+                        markers.push(L.marker([parseFloat(data[coords].lat), parseFloat(data[coords].lon)]).addTo(map));
+                    }
+                }
+
+                return this;
+            };
         }
-
-        map = L.map($objects.mapContainer[0], {
-            center: [51.06971, 13.77797],
-            zoom: 12
-        });
-
-        L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            subdomains: '1234'
-        }).addTo(map);
-    }
+    };
 
     return {
         createMap: function(){
