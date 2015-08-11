6b0536f9d1fa150035189c3f791b2f17d1bc0907
FireLizard <seb91gi@yahoo.de>
2015-08-11 20:08:38 +0200

    Add method 'getCoordsByQuery' to DataProviderInterface

--- src/Classes/Provider/DataProviderInterface.php
+++ src/Classes/Provider/DataProviderInterface.php
@@ -7,16 +7,16 @@ interface DataProviderInterface
     public function getData();
 
     /**
-     * Get all coords as array like: array([lat, lon], [lat, lon], ...).
+     * Get all coords.
      *
-     * @return array
+     * @return array An array like: array([lat, lon], [lat, lon], ...).
      */
     public function getAllCoords();
 
     /**
-     * Get content of all popups as array like: array(popup1, popup2, ...).
+     * Get content of all popups.
      *
-     * @return array
+     * @return array An array like: array(popup1, popup2, ...).
      */
     public function getAllPopupsContent();
 
@@ -26,4 +26,11 @@ interface DataProviderInterface
      * @return string
      */
     public function getFilterTemplate();
+
+    /**
+     * Get coords by a query object
+     * @param  mixed $query The query object.
+     * @return array An array like: array([lat, lon], [lat, lon], ...).
+     */
+    public function getCoordsByQuery($query);
 }
