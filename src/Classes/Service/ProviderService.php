28e77220881af6e5270efe16aca240ca6270ca23
Sebastian Gierth <seb91gi@yahoo.de>
2016-05-17 16:43:12 +0200

    [Improvement] add possibility to choose a filterProvider

---
+++ src/Classes/Service/ProviderService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Phoenix\Smartmap\Service;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Service to handle data providers.
+ */
+class ProviderService
+{
+    public static $getDataProvider_SIGNAL = 'getSmartMapDataProvider';
+    public static $getFilterProvider_SIGNAL = 'getSmartMapFilterProvider';
+
+    /**
+     * Get all data providers by dispatching a signal.
+     *
+     * @param $config
+     *
+     * @return array $config An array filled with \Phoenix\Smartmap\Provider\DataProviderInterface (if any exists).
+     */
+    public static function getDataProviders($config)
+    {
+        $config['items'][] = array('', '');
+
+        $dispatcher = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
+        $dispatcher->dispatch(__CLASS__, self::$getDataProvider_SIGNAL, $config);
+
+        return $config;
+    }
+
+    /**
+     * Get all data providers by dispatching a signal.
+     *
+     * @param $config
+     *
+     * @return array $config An array filled with \Phoenix\Smartmap\Provider\FilterProviderInterface (if any exists).
+     */
+    public static function getFilterProviders($config)
+    {
+        $config['items'][] = array('', '');
+
+        $dispatcher = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
+        $dispatcher->dispatch(__CLASS__, self::$getFilterProvider_SIGNAL, $config);
+
+        return $config;
+    }
+}
